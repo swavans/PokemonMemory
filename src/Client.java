3e7abffdbdfe27efe0870d520438a4ecf8c832ec
RickvF <pajfesse@avans.nl>
2016-06-08 14:59:39 +0200

    bijna

--- src/Client.java
+++ src/Client.java
@@ -1,5 +1,4 @@
 
-
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
@@ -11,15 +10,15 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashSet;
 import java.util.Iterator;
-import java.util.List;
 import java.util.Set;
 
 import javax.imageio.ImageIO;
@@ -28,103 +27,180 @@ import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.JTextArea;
 
 public class Client extends JFrame implements Runnable, Serializable
 {
 	private String host = "localhost";
-	
+
 	private DataInputStream fromServer;
 	private DataOutputStream toServer;
+	private ObjectInputStream fromServerObj;
+	private ObjectOutputStream toServerObj;
 	private String currentQuestionItem;
-	
+
 	JPanel contentPane;
 	JPanel pokePane;
 	JPanel buttonPane;
 	JPanel questionPane;
-	
-	private String pokemonChoose = "";
-	
-	private ArrayList<Pokemon> pokemons = new ArrayList();
+
+	private Pokemon pokemonChoose = null;
+
+	private volatile ArrayList<Pokemon> pokemons = new ArrayList();
 	private ArrayList<ArrayList> allQuestions = new ArrayList<>();
 	private ArrayList<String> types;
 	private ArrayList<String> types2;
 	private ArrayList<String> colors;
-	private ArrayList<String> generations; 
+	private ArrayList<String> generations;
 	private ArrayList<String> pokeNames;
 	private int selectedQuestionNumber;
-	
+
 	int i = 0;
-	
-	//de geselecteerde vraag die wordt verstuurd
+
+	private boolean myTurn = false;
+	private volatile int gameState = 0;
+	private Thread thread;
+	protected volatile boolean sendAnswer = false;
+	private volatile int gameStateP2 = 0;
+
+	// de geselecteerde vraag die wordt verstuurd
 	String selectQuestion = "";
-	
-	//setup for questionspane
+
+	// setup for questionspane
 	JComboBox<String> vragen;
-	JTextArea input;
-	
+
 	public static void main(String[] args)
 	{
 		new Client();
 
 	}
-	
+	public boolean getAnswer(){
+		return sendAnswer;
+	}
 	public Client()
 	{
-		//initialisation JFrame
+		// initialisation JFrame
 		super("Player");
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		
-		//setup JPanels
+
+		// setup JPanels
 		contentPane = new JPanel(new BorderLayout());
-		
+
 		pokePane = new JPanel(new GridLayout(5, 3));
 		buttonPane = new JPanel(new FlowLayout());
 		questionPane = new JPanel(new GridLayout(2, 1));
 		questionPane.setPreferredSize(new Dimension(400, 600));
-		
+
 		contentPane.add(pokePane, BorderLayout.CENTER);
 		contentPane.add(buttonPane, BorderLayout.SOUTH);
 		contentPane.add(questionPane, BorderLayout.EAST);
-		
+
 		connect();
-		
-		//setup data pokemon load
+
+		// setup data pokemon load
 		File f = new File("Files");
 		fill(f);
-		
-		//method to add the buttons 
+
+		// method to add the buttons
 		choosePokemon();
 		buttons();
 		questions();
-		
-		//initialisation JFrame
+
+		// initialisation JFrame
 		setContentPane(contentPane);
 		setVisible(true);
 		setResizable(false);
-		setSize(1024,600);
+		setSize(1024, 600);
 		setLocationRelativeTo(null);
-		
+
 	}
 
 	@Override
 	public void run()
 	{
-		// TODO Auto-generated method stub
 		
+		while(gameState == 0)
+		{
+			if(getAnswer())
+			{
+				try
+				{
+					gameStateP2 = fromServer.readInt();
+				} catch (IOException e2)
+				{
+					// TODO Auto-generated catch block
+					e2.printStackTrace();
+				}
+				
+				System.out.println("sA");
+				try
+				{
+					procesAnswer(fromServer.readBoolean());
+					if(pokemons.size() == 1)
+					{
+						gameState = 1;
+					}
+				} catch (IOException e)
+				{
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				sendAnswer =false;
+				myTurn = false;
+			}
+			
+			try
+			{
+				toServer.writeInt(gameState);
+			} catch (IOException e1)
+			{
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		}
+		
+		if(gameState == 1 || gameStateP2 == 2)
+		{
+			System.out.println("WINNER");
+			
+//			try
+//			{
+//				toServer.writeInt(gameState);
+//			} catch (IOException e1)
+//			{
+//				// TODO Auto-generated catch block
+//				e1.printStackTrace();
+//			}
+			
+		}
+		if(gameState == 2 || gameStateP2 == 1)
+		{
+			System.out.println("LOSE");
+			
+//			try
+//			{
+//				toServer.writeInt(gameState);
+//			} catch (IOException e1)
+//			{
+//				// TODO Auto-generated catch block
+//				e1.printStackTrace();
+//			}
+		}
 	}
-	
+
 	private void connect()
 	{
 		Socket socket;
 		try
 		{
 			socket = new Socket(host, 8000);
-			
-			//setup dataStream to and from server
+
+			// setup dataStream to and from server
 			fromServer = new DataInputStream(socket.getInputStream());
 			toServer = new DataOutputStream(socket.getOutputStream());
-			
+			fromServerObj = new ObjectInputStream(socket.getInputStream());
+			toServerObj = new ObjectOutputStream(socket.getOutputStream());
+			this.setTitle(fromServer.readUTF());
+
 		} catch (UnknownHostException e)
 		{
 			// TODO Auto-generated catch block
@@ -134,18 +210,22 @@ public class Client extends JFrame implements Runnable, Serializable
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+
+		thread = new Thread(this);
+		thread.start();
+
 	}
-	
+
 	private void choosePokemon()
 	{
-		//making the grid with buttons
-		for(int x = 0; x < 3; x++)
+		// making the grid with buttons
+		for (int x = 0; x < 3; x++)
 		{
-			for(int y = 0; y < 5; y++)
+			for (int y = 0; y < 5; y++)
 			{
-				if(i < 13)
-				{	
-					String name = pokemons.get(i).getName();
+				if (i < 13)
+				{
+					Pokemon p = pokemons.get(i);
 					Image img = null;
 					try
 					{
@@ -155,394 +235,384 @@ public class Client extends JFrame implements Runnable, Serializable
 						// TODO Auto-generated catch block
 						e2.printStackTrace();
 					}
-				
+
 					JButton b = new JButton(new ImageIcon(img.getScaledInstance(70, 100, Image.SCALE_SMOOTH)));
 					pokePane.add(b);
 					b.addActionListener(new ActionListener()
 					{
-						
+
 						@Override
 						public void actionPerformed(ActionEvent e)
 						{
-							pokemonChoose = name;
-							try {
-								System.out.println("send: " + pokemonChoose);
-								toServer.writeUTF(pokemonChoose);
-							} catch (IOException e1) {
+							pokemonChoose = p;
+							try
+							{
+								toServerObj.writeObject(pokemonChoose);
+							} catch (IOException e1)
+							{
 								// TODO Auto-generated catch block
 								e1.printStackTrace();
 							}
 						}
 					});
-					
+
 					i++;
 				}
-				
+
 			}
 		}
 	}
-	
-	//aanmaken van de buttons met hun listener
+
+	// aanmaken van de buttons met hun listener
 	private void buttons()
 	{
-		//setup send, quit, new game buttons
-		
+		// setup send, quit, new game buttons
+
 		JButton send = new JButton("Transfer Pokemon");
 		send.addActionListener(new ActionListener()
 		{
-			
+
 			@Override
 			public void actionPerformed(ActionEvent e)
 			{
-				procesAnswer(true);
-//				try
-//				{
-//					//send the question to the server
-//					toServer.writeUTF(selectQuestion);
-//				
-//
-//				} catch (IOException e1)
-//				{
-//					// TODO Auto-generated catch block
-//					e1.printStackTrace();
-//				}
-//				
-			
-				
+				try
+				{
+					myTurn = fromServer.readBoolean();
+					System.out.println("LOLZ");
+				} catch (IOException e2)
+				{
+					// TODO Auto-generated catch block
+					e2.printStackTrace();
+				}
+				if (myTurn)
+				{
+
+					try
+					{
+						sendAnswer = true;
+						System.out.println("TRUE");
+						toServer.writeUTF(selectQuestion);
+
+					} catch (IOException e1)
+					{
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+
+				}
 			}
 		});
-		
+
 		JButton quit = new JButton("QUIT");
 		quit.addActionListener(new ActionListener()
 		{
-			
+
 			@Override
 			public void actionPerformed(ActionEvent e)
 			{
 				System.exit(0);
-				
+
 			}
 		});
-		
+
 		JButton ngame = new JButton("NEW GAME");
 		ngame.addActionListener(new ActionListener()
 		{
-			
+
 			@Override
 			public void actionPerformed(ActionEvent e)
 			{
 				// TODO Auto-generated method stub
-				
+
 			}
 		});
-		
-		//add buttons to his JPanel
+
+		// add buttons to his JPanel
 		buttonPane.add(ngame);
 		buttonPane.add(quit);
 		buttonPane.add(send);
 	}
-	
+
 	private void questions()
 	{
-		//setup questionsbox
+		// setup questionsbox
 		vragen = new JComboBox<String>();
 		vragen.addActionListener(new ActionListener()
 		{
-			
+
 			@Override
 			public void actionPerformed(ActionEvent e)
 			{
-				//System.out.println((String)allQuestions.get(vragen.getSelectedIndex()).get(1));
-				//de geselecteerde vraag toewijzen aan de variabele zodat deze kan worden verstuurd
-				selectedQuestionNumber = vragen.getSelectedIndex();
-				if(allQuestions.size()>0)
+			selectedQuestionNumber = vragen.getSelectedIndex();
+				if (allQuestions.size() > 0)
 				{
-					selectQuestion = (String)allQuestions.get(selectedQuestionNumber).get(1);
+					selectQuestion = (String) allQuestions.get(selectedQuestionNumber).get(1);
 
 				}
 			}
 		});
 		questionPane.add(vragen);
-		
-		//setup dialog
-		input = new JTextArea();
-		questionPane.add(input);
+
+		// setup dialog
+
 		randomFillCombo();
 	}
-	
 
-	//het vullen van de combobox met de vragen die worden getoond
+	// het vullen van de combobox met de vragen die worden getoond
 	private void randomFillCombo()
 	{
-		if(pokemons.size()<1)
-			System.out.println("No pokemons");
-		for(Pokemon pokemon : pokemons)
-			System.out.println(pokemon.toString());
+		
+
 		allQuestions.clear();
 		vragen.removeAllItems();
-		for(int i = 0; i<5; i++)
+		for (int i = 0; i < 5; i++)
 		{
-			int g = (int)( Math.random() * 4); 
+			int g = (int) (Math.random() * 4);
 			switch (g)
 			{
-			//names
+			// names
 			case 0:
-				if(pokeNames.size() > 0){
-					String name = (String) pokeNames.get((int) (Math.random()*(pokeNames.size())));
-				
-				ArrayList<String> question = new ArrayList<>();
-				question.add("Is deze Pokemon ");
-				question.add(name);
-				question.add("name");
-				allQuestions.add(question);
-				}
-				else
-					System.out.println("Clear Names");
+				if (pokeNames.size() > 0)
+				{
+					String name = (String) pokeNames.get((int) (Math.random() * (pokeNames.size())));
+
+					ArrayList<String> question = new ArrayList<>();
+					question.add("Is deze Pokemon ");
+					question.add(name);
+					question.add("name");
+					allQuestions.add(question);
+				} break;
+				// types
+			case 1:
+				if (types.size() > 0)
+				{
+					String type = (String) types.get((int) (Math.random() * (types.size() - 1)));
+					ArrayList<String> question2 = new ArrayList<>();
+					question2.add("Is het type van de Pokemon ");
+					question2.add(type);
+					question2.add("type");
+					allQuestions.add(question2);
+				} 
 				break;
-			//types
-			case 1: 
-				if(types.size()>0){
-				String type = (String) types.get((int) (Math.random()*(types.size()-1)));
-				ArrayList<String> question2 = new ArrayList<>();
-				question2.add("Is het type van de Pokemon ");
-				question2.add(type);
-				question2.add("type");
-				allQuestions.add(question2);
-				}
-				else
-					System.out.println("Clear Types");
+				// colors
+			case 2:
+				if (colors.size() > 0)
+				{
+					String color = (String) colors.get((int) (Math.random() * (colors.size() - 1)));
+					ArrayList<String> question3 = new ArrayList<>();
+					question3.add("Is de kleur van deze pokemon ");
+					question3.add(color);
+					question3.add("color");
+					allQuestions.add(question3);
+				} 
 				break;
-			//colors	
-			case 2: 
-				if(colors.size()>0){
-				String color = (String) colors.get((int)(Math.random()*(colors.size()-1)));
-				ArrayList<String> question3 = new ArrayList<>();
-				question3.add("Is de kleur van deze pokemon ");
-				question3.add(color);
-				question3.add("color");
-				allQuestions.add(question3);
-				}
-				else
-					System.out.println("Clear Colors");
-				break;
-			//generations
+				// generations
 			case 3:
-				if(generations.size()>0){
-				String gen = (String) generations.get((int)(Math.random()*(generations.size()-1)));
-				ArrayList<String> question4 = new ArrayList<>();
-				question4.add("Is deze pokemon van generatie ");
-				question4.add(gen);
-				question4.add("generation");
-				allQuestions.add(question4);
-				}
-				else
-					System.out.println("Clear generations");
-				break;			
+				if (generations.size() > 0)
+				{
+					String gen = (String) generations.get((int) (Math.random() * (generations.size() - 1)));
+					ArrayList<String> question4 = new ArrayList<>();
+					question4.add("Is deze pokemon van generatie ");
+					question4.add(gen);
+					question4.add("generation");
+					allQuestions.add(question4);
+				} 
+				break;
 			}
 		}
-		for(ArrayList<String> list: allQuestions)
+		for (ArrayList<String> list : allQuestions)
 		{
-			String vraag ="";
-			for(int p = 0; p<list.size()-1; p++)
+			String vraag = "";
+			for (int p = 0; p < list.size() - 1; p++)
 			{
-				vraag = vraag+list.get(p);
+				vraag = vraag + list.get(p);
 			}
 			vraag = vraag + " ?";
 			vragen.addItem(vraag);
 		}
-		selectQuestion = vragen.getItemAt(0);
+		 selectQuestion = vragen.getItemAt(0);
 	}
-	
-	//ophalen van alle .pkb files en deze omzetten naar Pokemon objects
+
+	// ophalen van alle .pkb files en deze omzetten naar Pokemon objects
 	private void fill(File file)
 	{
-		Set<String> pNames= new HashSet<>();
-		Set<String> pTypes= new HashSet<>();
+		Set<String> pNames = new HashSet<>();
+		Set<String> pTypes = new HashSet<>();
 		Set<String> pGen = new HashSet<>();
 		Set<String> pColor = new HashSet<>();
-		System.out.println("search pkb files");
-		//search .pkb files 
-		if(file.exists())
+		// search .pkb files
+		if (file.exists())
 		{
-			System.out.println(file);
-			if(file.isDirectory())
+			if (file.isDirectory())
 			{
-				System.out.println(file);
 				File[] files = file.listFiles();
-				for(int i = 0; files != null && i < files.length; i++)
+				for (int i = 0; files != null && i < files.length; i++)
 				{
-					if(files[i].isDirectory())
+					if (files[i].isDirectory())
 					{
 						fill(files[i]);
 					}
-					if(getFileExtension(files[i]).equals("pkb"))
+					if (getFileExtension(files[i]).equals("pkb"))
 					{
-						//TODO
+						// TODO
 						Pokemon p = new Pokemon(files[i].getName());
 						pokemons.add(p);
-						
+
 						pNames.add(p.getName());
-						
-						for(String type : p.getTypes())
+
+						for (String type : p.getTypes())
 						{
 							pTypes.add(type);
 						}
-						
+
 						pColor.add(p.getColor());
-						
-						pGen.add(p.getGeneration()+ "");
-						
+
+						pGen.add(p.getGeneration() + "");
+
 					}
 				}
 			}
 		}
 		Collections.sort(pokemons, Pokemon.GenComparator);
-		pokeNames = new ArrayList<>( pNames);
+		pokeNames = new ArrayList<>(pNames);
 		types = new ArrayList<>(pTypes);
 		types2 = new ArrayList<>(types);
 		generations = new ArrayList<>(pGen);
 		colors = new ArrayList<>(pColor);
-		
-		
+
 	}
-	
-	//opvragen wat de extension is van een object
-	public String getFileExtension(File file) 
+
+	// opvragen wat de extension is van een object
+	public String getFileExtension(File file)
 	{
-		//search fileextension of the file
-        String fileName = file.getName();
-        if(fileName.lastIndexOf(".") != -1 && fileName.lastIndexOf(".") != 0)
-        return fileName.substring(fileName.lastIndexOf(".")+1);
-        else return "";
-    }
-	
+		// search fileextension of the file
+		String fileName = file.getName();
+		if (fileName.lastIndexOf(".") != -1 && fileName.lastIndexOf(".") != 0)
+			return fileName.substring(fileName.lastIndexOf(".") + 1);
+		else
+			return "";
+	}
+
 	public void procesAnswer(boolean answer)
 	{
-		switch((String)allQuestions.get(selectedQuestionNumber).get(2))
+		switch ((String) allQuestions.get(selectedQuestionNumber).get(2))
 		{
 		case ("name"):
-			if(answer)
+			if (answer)
 			{
 				procesAnswerTrue(pokeNames);
-			}
-			else
+			} else
 				procesAnswerFalse(pokeNames);
 			break;
 		case ("type"):
-			if(answer)
+			if (answer)
 			{
 				procesAnswerTrue(types);
-			}
-			else
+			} else
 				procesAnswerFalse(types);
 			break;
 		case ("generation"):
-			if(answer)
+			if (answer)
 			{
 				procesAnswerTrue(generations);
-			}
-			else
-				procesAnswerFalse(generations);		
+			} else
+				procesAnswerFalse(generations);
 			break;
 		case "color":
-			if(answer)
+			if (answer)
 			{
 				procesAnswerTrue(colors);
-			}
-			else
+			} else
 				procesAnswerFalse(colors);
-			break;			
+			break;
 		}
-		
+		randomFillCombo();
 		createNewPokemonList(answer);
 		generateList();
-		//if(pokeNames.size()>0 || types.size()>0|| generations.size()>0||colors.size()>0 )
-		randomFillCombo();
-		System.out.println("Succes");
-	}
-	
+		// if(pokeNames.size()>0 || types.size()>0||
+		// generations.size()>0||colors.size()>0 )
+		}
+
 	private void procesAnswerTrue(ArrayList<String> list)
 	{
 		list.clear();
 		list.add(selectQuestion);
 	}
-	
+
 	private void procesAnswerFalse(ArrayList<String> list)
 	{
 		Iterator<String> itr = list.iterator();
-		while(itr.hasNext())
+		while (itr.hasNext())
 		{
 			String s = itr.next();
-			if(s.equals(selectQuestion))
+			if (s.equals(selectQuestion))
 			{
 				itr.remove();
 			}
 		}
 	}
-	
+
 	private void createNewPokemonList(boolean answer)
 	{
 		String iets = selectQuestion;
 		Iterator<Pokemon> pokeitr = pokemons.iterator();
-		
-		while(pokeitr.hasNext())
+
+		while (pokeitr.hasNext())
 		{
 			Pokemon p = pokeitr.next();
-			
-			if(!(pokeNames.contains(p.getName())))
-			{
-				pokeitr.remove();
-			}
-			
-			else if(!(generations.contains((p.getGeneration() + ""))))
-			{
-				pokeitr.remove();
-			}
-			
-			else if(!(colors.contains(p.getColor())))
-			{
-				pokeitr.remove();
-			}
-			
 
-			if(types2.contains(selectQuestion))
+			if (!(pokeNames.contains(p.getName())))
+			{
+				pokeitr.remove();
+			}
+
+			else if (!(generations.contains((p.getGeneration() + ""))))
+			{
+				pokeitr.remove();
+			}
+
+			else if (!(colors.contains(p.getColor())))
+			{
+				pokeitr.remove();
+			}
+
+			if (types2.contains(selectQuestion))
 			{
 				ArrayList<String> t3 = new ArrayList<>(types);
-				if((!(p.getTypes().contains(selectQuestion))) && answer)
+				if ((!(p.getTypes().contains(selectQuestion))) && answer)
 					pokeitr.remove();
-				else if(p.getTypes().contains(selectQuestion) && !answer)
+				else if (p.getTypes().contains(selectQuestion) && !answer)
 					pokeitr.remove();
 			}
-			
+
 		}
 
-		
 	}
-	
+
 	private void generateList()
 	{
-		Set<String> pNames= new HashSet<>();
-		Set<String> pTypes= new HashSet<>();
+		Set<String> pNames = new HashSet<>();
+		Set<String> pTypes = new HashSet<>();
 		Set<String> pGen = new HashSet<>();
 		Set<String> pColor = new HashSet<>();
-		for(Pokemon p : pokemons)
+		for (Pokemon p : pokemons)
 		{
 			pNames.add(p.getName());
-			
-			for(String type : p.getTypes())
+
+			for (String type : p.getTypes())
 			{
 				pTypes.add(type);
 			}
-			
+
 			pColor.add(p.getColor());
-			
-			pGen.add(p.getGeneration()+ "");
+
+			pGen.add(p.getGeneration() + "");
 		}
-		pokeNames = new ArrayList<>( pNames);
+		pokeNames = new ArrayList<>(pNames);
 		types = new ArrayList<>(pTypes);
 		types2 = new ArrayList<>(types);
 		generations = new ArrayList<>(pGen);
 		colors = new ArrayList<>(pColor);
-		
+
 	}
 }
--- src/Server.java
+++ src/Server.java
@@ -1,8 +1,9 @@
 
-
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -14,79 +15,87 @@ public class Server extends JFrame
 	{
 		new Server();
 	}
-	
+
 	public Server()
 	{
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	    setSize(300, 300);
-	    setTitle("Server");
-	    setVisible(true);
-	    
+		setSize(300, 300);
+		setTitle("Server");
+		setVisible(true);
+
 		try
 		{
-			//setup server
+			// setup server
 			ServerSocket server = new ServerSocket(8000);
-			
-			while(true)
+
+			while (true)
 			{
-				//accept player1
+				// accept player1
 				Socket player1 = server.accept();
-				//accept player2
+				// accept player2
 				Socket player2 = server.accept();
-				
-				//setup class which handle a who is it game
+
+				// setup class which handle a who is it game
 				HandleAGame game = new HandleAGame(player1, player2);
-				
-				//setup thread
+
+				// setup thread
 				new Thread(game).start();
 			}
-		}
-		catch(IOException ex)
+		} catch (IOException ex)
 		{
 			ex.printStackTrace();
 		}
 	}
-	
+
 	class HandleAGame implements Runnable
 	{
 		private Socket player1;
 		private Socket player2;
-		
-		//setup dataStream player1 and player2
+
+		// setup dataStream player1 and player2
 		private DataInputStream fromPlayer1;
 		private DataOutputStream toPlayer1;
+		private ObjectInputStream fromPlayer1Obj;
+		private ObjectOutputStream toPlayer1Obj;
 		private DataInputStream fromPlayer2;
 		private DataOutputStream toPlayer2;
+		private ObjectInputStream fromPlayer2Obj;
+		private ObjectOutputStream toPlayer2Obj;
+
+		private Pokemon player1P = null;
+		private Pokemon player2P = null;
+		private int turn = 0;
 		
-		//Constructor 
+		private int gameS1 =0;
+		private int gameS2 = 0;
+
+		// Constructor
 		public HandleAGame(Socket player1, Socket player2)
 		{
 			this.player1 = player1;
 			this.player2 = player2;
 			
+			// initialisation dataStream for player1
 			try
 			{
-				//initialisation dataStream for player1
 				toPlayer1 = new DataOutputStream(player1.getOutputStream());
 				fromPlayer1 = new DataInputStream(player1.getInputStream());
-				
-				//initialisation dataStream for player2
+				toPlayer1Obj = new ObjectOutputStream(player1.getOutputStream());
+				fromPlayer1Obj = new ObjectInputStream(player1.getInputStream());
+				toPlayer1.writeUTF("Player1");
+				// initialisation dataStream for player2
 				toPlayer2 = new DataOutputStream(player2.getOutputStream());
 				fromPlayer2 = new DataInputStream(player2.getInputStream());
-				
-				
-				//playing the game
-				while(true)
-				{
-					//TODO afhandeling van het spel
-					
-				}
-				
-			} catch (IOException e)
-			{
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+				toPlayer2Obj = new ObjectOutputStream(player2.getOutputStream());
+				fromPlayer2Obj = new ObjectInputStream(player2.getInputStream());
+				toPlayer2.writeUTF("Player2");
+
 			}
+			catch(Exception e)
+			{
+				
+			}
+
 		}
 
 		@Override
@@ -94,25 +103,119 @@ public class Server extends JFrame
 		{
 			try
 			{
-				//initialisation dataStream for player1
-				fromPlayer1 = new DataInputStream(player1.getInputStream());
-				toPlayer1 = new DataOutputStream(player1.getOutputStream());
-				
-				//initialisation dataStream for player2
-				fromPlayer2 = new DataInputStream(player2.getInputStream());
-				toPlayer2 = new DataOutputStream(player2.getOutputStream());
-				
-				//playing the game
-				while(true)
+				// playing the game
+				while (true)
 				{
-					//TODO afhandeling van het spel
+					if(player1P == null || player2P == null)
+					{
+						if (player1P == null)
+						{
+							try
+							{
+								player1P = (Pokemon) fromPlayer1Obj.readObject();
+							
+							} catch (ClassNotFoundException e)
+							{
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							} finally
+							{
+								//System.out.println("p1: " + player1P.toString());
+							}
+						}
+						if (player2P == null)
+						{
+							try
+							{
+								player2P = (Pokemon) fromPlayer2Obj.readObject();
+							} catch (ClassNotFoundException e)
+							{
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							} finally
+							{
+								//System.out.println("p2: " + player2P.toString());
+							}
+						}
+					}
 					
+					else
+					{
+
+						turn++;
+						if (turn % 2 == 1)
+						{
+							
+							
+							String question = null;
+							// set turn true player 1
+							toPlayer1.writeBoolean(true);
+							
+							toPlayer1.writeInt(gameS2);
+							//System.out.println("send turn p1");
+							while (question == null)
+							{
+								question = fromPlayer1.readUTF();
+								
+							}
+							//System.out.println(question);
+							if (question != null)
+							{
+								toPlayer1.writeBoolean(checkAwnser(question, player2P));
+								//System.out.println(checkAwnser(question, player2P));
+							}
+							
+							gameS1 = fromPlayer1.readInt();
+							
+						} else
+						{
+							
+							
+							String question = null;
+							// set turn true player 1
+							toPlayer2.writeBoolean(true);
+							
+							toPlayer2.writeInt(gameS1);
+							while (question == null)
+							{
+								question = fromPlayer2.readUTF();
+							}
+							if (question != null)
+							{
+								toPlayer2.writeBoolean(checkAwnser(question, player1P));
+
+							}
+							
+							gameS2 = fromPlayer2.readInt();
+							
+						}
+					}
+
 				}
-				
+
 			} catch (IOException e)
 			{
 				// TODO Auto-generated catch block
 				e.printStackTrace();
+			}
+		}
+
+		private boolean checkAwnser(String question, Pokemon p)
+		{
+			for (String t : p.getTypes())
+			{
+				if (t.equals(question))
+				{
+					return true;
+				}
+			}
+			if (question.equals(p.getName()) || question.equals(p.getColor())
+					|| question.equals(p.getGeneration() + ""))
+			{
+				return true;
+			} else
+			{
+				return false;
 			}
 		}
 	}
